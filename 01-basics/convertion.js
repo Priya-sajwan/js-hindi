b529a9bbc09238d55862264e42aba042bba0e48a
Priya-Sajwan <priya.sajwan101@gmail.com>
2026-07-20 08:11:56 +0000

    change in document of file

--- 01-basics/convertion.js
+++ 01-basics/convertion.js
@@ -1,4 +1,22 @@
-let score = 89
-let numb = Number(score)
-console.log(typeof numb);
+// let score = 89
+// let numb = Number(score)
+// console.log(typeof numb);
+ 
+// let tru = true
+// let boo = Boolean(tru)
+// console.log( typeof boo)
 
+
+// console.log("2" >= 2);
+// console.log("2" === 2);
+
+let myfile = ["yoy", "yoyo", "yoyoyo"]
+console.log(myfile);
+
+let docm = {
+    name: "priya",
+    age :"20",
+    caste : "sajwan",
+}
+
+console.log(docm);
---
+++ 01-basics/variables.js
@@ -0,0 +1,13 @@
+const accountId = 1233
+let accountEmail = "priya@gmail.com"
+var accountPassword = "3456"
+accountCity = "jaipur"
+ 
+// accountId = 3// not allowed 
+
+console.log(accountId)
+
+
+console.log(accountEmail)
+
+
