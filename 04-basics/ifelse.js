d0784f4a464db622cd566becf2ecf488181d574f
Priya-Sajwan <priya.sajwan101@gmail.com>
2026-07-31 05:28:17 +0530

    chnage in location of file

--- 04-basics/ifelse.js
+++ 04-basics/ifelse.js
@@ -1,2 +1,39 @@
 //if 
 
+// const temp = 24
+
+// if ( temp < 43){
+// console.log(" yes temp is less than 43");
+// }
+
+// else {console.log("no ,temp is not less than 43 ")}
+ 
+
+// ++++++++++++++++++++++++++++++ scope ++++++++++++++++++++++++++++++++
+// const score = 67
+// if (score > 45){
+//     let power = "fly"
+//     console.log(power);
+
+// }
+// console.log(power);     //  it does not execute bcz it is out of  the scope.
+
+//++++++++++++++++++++++++++++++++ direct (immature  [do not do this])+++++++++++++++++++++++++++++++
+// const condition = 1000
+// if (condition >500) console.log("yoyo"), console.log("again yoyo")
+
+// +++++++++++++++++++++==== and or or +++++++++++++++++++++++++
+
+// and 
+
+// const logIn = true 
+// const mobile = false
+// const laptop = true 
+//  if ( logIn && mobile){
+//     console.log("access to your account")
+//  }
+// or
+
+//  if ( mobile || laptop){
+//     console.log("ok")
+//  }
---
+++ 04-basics/switch.js
@@ -0,0 +1,31 @@
+//switch
+
+// const month = 3
+// switch(month) {
+//     case 1:
+//         console.log("jan")
+//         break;
+//     case 2:
+//         console.log("feb")
+//         break;
+//     case 3:
+//         console.log("march")
+//         break;
+//     case 4:
+//         console.log("april")
+//         break;
+//     case 5:
+//         console.log("may")
+//         break;
+//     default:
+//         console.log("nothing is here")
+//         break;
+// }
+
+
+const emailId = "priya@gamil.com"
+if (emailId.length == 0){
+    console.log("empty")
+}
+console.log("allow")
+
